c653d96388bb791490d10e0d94747f15f6eb5560
akarui-shi <mmar1yakucherova@yandex.ru>
2024-03-25 15:20:10 +0300

    fix: ErrorHandler

--- src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.error;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -28,6 +29,13 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handlerException(final Exception exception) {
+        log.info("{}", exception.getMessage());
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handlerMethodArgumentNotValidException(final MethodArgumentNotValidException exception) {
         log.info("{}", exception.getMessage());
         return new ErrorResponse(exception.getMessage());
     }
